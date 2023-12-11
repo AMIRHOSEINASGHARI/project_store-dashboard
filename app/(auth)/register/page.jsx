d15a7487800367feee0497c8c01a62160078364f
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-12-12 03:06:29 +0330

    securing auth pages and home page in case of authorization

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -1,6 +1,12 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import AuthForm from "@/components/auth/AuthForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const Register = () => {
+const Register = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect("/");
   return <AuthForm type="register" />;
 };
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,11 @@
-const Home = () => {
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) redirect("/login");
   return <div>Home</div>;
 };
 
