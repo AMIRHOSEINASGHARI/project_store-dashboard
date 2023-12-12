5fdbdbd574c00f6e8011a30d961adeb30ce2f087
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-12-12 16:19:21 +0330

    session context provider created

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import MainContextProvider from "@/context/MainContextProvider";
+import SessionContextProvider from "@/context/SessionContextProvider";
 
 export const metadata = {
   title: "Store Panel",
@@ -9,14 +10,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <MainContextProvider>
-        <body>
-          <div>
-            <Toaster position="top-center" />
-          </div>
-          <main>{children}</main>
-        </body>
-      </MainContextProvider>
+      <SessionContextProvider>
+        <MainContextProvider>
+          <body>
+            <div>
+              <Toaster position="top-center" />
+            </div>
+            <main>{children}</main>
+          </body>
+        </MainContextProvider>
+      </SessionContextProvider>
     </html>
   );
 }
