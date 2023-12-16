0869a917cc94c7314cdb91662084f317341dbaf0
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-12-16 07:17:56 +0330

    nothing

--- app/page.jsx
+++ app/page.jsx
@@ -2,17 +2,11 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import DashboardPage from "@/components/pages/dashboard/DashboardPage";
-import StoreDashboardUser from "@/utils/models/user";
-import NotAllowed from "@/components/shared/NotAllowed";
 
 const Home = async () => {
   const session = await getServerSession(authOptions);
-  const currentUser = await StoreDashboardUser.findOne({
-    username: session?.user?.email,
-  }).select(["roll"]);
 
   if (!session) redirect("/login");
-  if (currentUser?.roll === "USER") return <NotAllowed />;
   return <DashboardPage />;
 };
 
