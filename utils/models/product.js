1d4d1c7ee49a28dd149b4962eaaab0f2f8c6eca9
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-12-18 21:02:56 +0330

    product model

---
+++ utils/models/product.js
@@ -0,0 +1,28 @@
+const { Schema, models, model } = require("mongoose");
+
+const productSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  discount: { type: Number, default: 0 },
+  category: { type: String, required: true },
+  colors: { type: [String], required: true },
+  keywords: { type: [String], required: true },
+  likes: [
+    { type: Schema.Types.ObjectId, ref: "StoreDashboardUser", default: [] },
+  ],
+  comments: [
+    { type: Schema.Types.ObjectId, ref: "StoreDashboardComment", default: [] },
+  ],
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutabale: true,
+  },
+});
+
+export const StoreDashboardProduct =
+  models?.StoreDashboardProduct ||
+  model("StoreDashboardProduct", productSchema);
