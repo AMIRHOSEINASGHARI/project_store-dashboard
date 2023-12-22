9546a85f2b30d7fb88ffa47b83bd340d432402de
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-12-22 08:53:34 +0330

    latest products api for store

---
+++ app/api/store/products/latest/route.js
@@ -0,0 +1,28 @@
+import connectDB from "@/utils/connectDB";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log("Cannot connect to DB!", error);
+    return NextResponse.json(
+      { msg: "Server Error!", success: false },
+      { status: 500 }
+    );
+  }
+
+  try {
+    const products = await StoreDashboardProduct.find().sort({ createdAt: -1 });
+
+    return NextResponse.json(
+      { msg: "OK", success: true, data: products.slice(0, 10) },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { msg: "Server Error!", success: false },
+      { status: 500 }
+    );
+  }
+}
