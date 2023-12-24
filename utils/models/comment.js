668a7f64c3acde3c84d699c683bd00f5067ccd2a
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-12-24 23:35:16 +0330

    ...

--- utils/models/comment.js
+++ utils/models/comment.js
@@ -3,6 +3,7 @@ const { Schema, models, model } = require("mongoose");
 const commentSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
+  answer: { type: String, default: "" },
   productId: { type: Schema.Types.ObjectId, ref: "StoreDashboardProduct" },
   senderId: { type: Schema.Types.ObjectId, ref: "StoreShopUser" },
   createdAt: {
