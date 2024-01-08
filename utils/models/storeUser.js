1518e6295b9fd0f948890ad7fcc66ef0a4bd2d69
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-01-08 20:21:57 +0330

    changing store user model

--- utils/models/storeUser.js
+++ utils/models/storeUser.js
@@ -1,9 +1,17 @@
 const { Schema, models, model } = require("mongoose");
 
 const userSchema = new Schema({
-  username: String,
-  displayName: String,
-  password: String,
+  username: { type: String, required: true, unique: true },
+  displayName: { type: String, required: true },
+  password: { type: String, required: true },
+  avatar: { type: String, default: "" },
+  phoneNumber: { type: Number, default: 0 },
+  address: { type: String, default: "" },
+  orders: [{ type: Schema.Types.ObjectId, ref: "StoreShopOrder", default: [] }],
+  likes: [{ type: Schema.Types.ObjectId, ref: "StoreShopLike", default: [] }],
+  comments: [
+    { type: Schema.Types.ObjectId, ref: "StoreShopComment", default: [] },
+  ],
   createdAt: {
     type: Date,
     default: () => Date.now(),
