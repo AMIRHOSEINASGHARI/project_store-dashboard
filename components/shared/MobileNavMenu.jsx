ea6f45763b112a38088e98e263bd0014b30e04e5
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-04-03 00:17:10 +0330

    MobileNav icons bug fixed

--- components/shared/MobileNavMenu.jsx
+++ components/shared/MobileNavMenu.jsx
@@ -9,9 +9,31 @@ import { Fragment } from "react";
 import { signOut } from "next-auth/react";
 import { usePathname } from "next/navigation";
 import { AiOutlineClose } from "react-icons/ai";
+import { PiPowerThin } from "react-icons/pi";
 
 const MobileNav = ({ onClose, open, setOpen }) => {
   const pathname = usePathname();
+
+  const _drawer = {
+    styles: {
+      body: { padding: "0px", margin: "10px 0px" },
+      header: { padding: "15px 20px" },
+    },
+    title: (
+      <div className="flex items-center justify-between">
+        <Link href="/" className="flex items-center gap-[10px]">
+          <Image src="/logo1.svg" width={35} height={35} alt="logo" priority />
+          <div className="flex items-center italic font-bold">
+            <span className="text-violet-600">Online</span>
+            <span>Shop</span>
+          </div>
+        </Link>
+        <button onClick={() => onClose()}>
+          <AiOutlineClose className="text-[25px] text-gray-700" />
+        </button>
+      </div>
+    ),
+  };
 
   return (
     <div>
@@ -22,32 +44,12 @@ const MobileNav = ({ onClose, open, setOpen }) => {
         <HiMenuAlt2 className="w-[25px] h-[25px]" />
       </button>
       <Drawer
-        bodyStyle={{ padding: "0px" }}
         placement="left"
         onClose={onClose}
         open={open}
         closeIcon={false}
-        headerStyle={{ padding: "15px 20px" }}
-        title={
-          <div className="flex items-center justify-between">
-            <Link href="/" className="flex items-center gap-[10px]">
-              <Image
-                src="/logo1.svg"
-                width={35}
-                height={35}
-                alt="logo"
-                priority
-              />
-              <div className="flex items-center italic font-bold">
-                <span className="text-violet-600">Online</span>
-                <span>Shop</span>
-              </div>
-            </Link>
-            <button onClick={() => onClose()}>
-              <AiOutlineClose className="text-[30px]" />
-            </button>
-          </div>
-        }
+        styles={_drawer.styles}
+        title={_drawer.title}
       >
         <nav>
           <ul>
@@ -57,22 +59,15 @@ const MobileNav = ({ onClose, open, setOpen }) => {
                   onClick={() => onClose()}
                   className={`rounded-l-[10px] mx-[20px] my-[5px] transition duration-75 ease-in-out mb-[2px] ${
                     pathname === item.link
-                      ? "bg-gray-100 font-bold border-r-4 border-gray-500"
-                      : "bg-white hover:bg-gray-100 font-semibold"
+                      ? "bg-gray-100 text-purple-500 font-bold border-r-4 border-purple-500"
+                      : "font-light"
                   }`}
                 >
                   <Link
                     href={item.link}
                     className="flex items-center gap-[20px] px-[10px] py-[12px]"
                   >
-                    <Image
-                      src={item.image}
-                      alt={item.title}
-                      width={20}
-                      height={20}
-                      priority
-                      className="w-[20px]"
-                    />
+                    <div className="text-[22px]">{item.image}</div>
                     <span class="text-[13px]">{item.title}</span>
                   </Link>
                 </li>
@@ -84,7 +79,7 @@ const MobileNav = ({ onClose, open, setOpen }) => {
                 )}
               </Fragment>
             ))}
-            <li className="rounded-l-[10px] mx-4 my-1 hover:bg-gray-100 transition duration-75 ease-in-out">
+            <li className="bg-red-50 text-red-600 rounded-[10px] mx-[20px] my-[5px] hover:bg-gray-100 transition duration-75 ease-in-out">
               <button
                 onClick={() => {
                   signOut();
@@ -92,14 +87,9 @@ const MobileNav = ({ onClose, open, setOpen }) => {
                 }}
                 className="flex items-center w-full gap-[20px] p-[10px]"
               >
-                <Image
-                  src="/icons/power.svg"
-                  width={20}
-                  height={20}
-                  alt="logout"
-                  priority
-                  className="w-[20px]"
-                />
+                <div className="text-[22px]">
+                  <PiPowerThin />
+                </div>
                 <span class="text-[13px] font-semibold">Exit</span>
               </button>
             </li>
